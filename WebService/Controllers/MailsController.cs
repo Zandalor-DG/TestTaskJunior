96613371ea3f7a7e7841ad55134a457e57d41892
Zandalor-DG <Paliy.zdg@gmail.com>
2020-09-02 16:50:07 +0300

    Add function SendEmailAsync

--- WebService/Controllers/MailsController.cs
+++ WebService/Controllers/MailsController.cs
@@ -3,7 +3,9 @@
     #region << Using >>
 
     using System.Threading.Tasks;
+    using MailKit.Net.Smtp;
     using Microsoft.AspNetCore.Mvc;
+    using MimeKit;
     using WebService.Model;
 
     #endregion
@@ -27,9 +29,27 @@
 
         #endregion
 
-        [HttpPost]
-        public Task<IActionResult> Post(EmailPostModel email)
+        public async Task<IActionResult> SendEmailAsync(string email, string subject, string message)
         {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "Chamar007@yandex.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+                                {
+                                        Text = message
+                                };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.yandex.com", 465, true);
+                await client.AuthenticateAsync("Chamar007@yandex.ru", "@123654789@");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+
             return Ok();
         }
     }
